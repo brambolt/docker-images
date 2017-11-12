cd4bcf930c50b61c16001edc0e74b82869e9cbe4
Stefán Sigurðsson <stefan.sigurdsson@brambolt.com>
2017-11-12 11:01:10 +0100

    No volume container change

--- bin/build-image-oracle-ee-no-volume.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-(cd ./OracleDatabase/dockerfiles-no-volume/ ; ./buildDockerImage.sh -e -v 12.2.0.1)
-
---
+++ bin/build-image-oracle-ee-novolume.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+(cd ./OracleDatabase/dockerfiles-novolume/ ; ./buildDockerImage.sh -e -v 12.2.0.1)
+
